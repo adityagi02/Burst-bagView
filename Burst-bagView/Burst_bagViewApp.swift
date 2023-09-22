beb60cfdb7aad896978a7f145ef6b48348e542fb
Aditya Tyagi <adityatyagi20bcs126@iiitkottayam.ac.in>
2023-09-22 14:55:41 +0530

    Initial Commit

---
+++ Burst-bagView/Burst_bagViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Burst_bagViewApp.swift
+//  Burst-bagView
+//
+//  Created by Aditya Tyagi  on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Burst_bagViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
